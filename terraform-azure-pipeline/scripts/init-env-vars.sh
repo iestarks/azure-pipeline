226add6b174f23f7e83d7acb629a666efa3954fd
Irving <ncsu.ee2017@gmail.com>
2020-02-09 11:27:01 -0500

    updating javatest

--- terraform-azure-pipeline/scripts/init-env-vars.sh
+++ terraform-azure-pipeline/scripts/init-env-vars.sh
@@ -2,8 +2,8 @@
 
 set -e
 
-export LOCATION=westeurope
-export COMMON_RESOURCE_GROUP_NAME=terraform-ref-rg
-export TF_STATE_STORAGE_ACCOUNT_NAME=tfstate2195
-export TF_STATE_CONTAINER_NAME=tfstate-ref
-export KEYVAULT_NAME=terraform-ref-kv
+export LOCATION=eastus2
+export COMMON_RESOURCE_GROUP_NAME=az-terraform-ref-rg
+export TF_STATE_STORAGE_ACCOUNT_NAME=tfstate8e1ab
+export TF_STATE_CONTAINER_NAME=tfstate-ref-bucket
+export KEYVAULT_NAME=terraform-ref-8e1abkv
